4fa621932bbd7b1b28de9ce3837e5c40bc441aa0
hastingsaustin <hastings.austin@student.ccm.edu>
2026-09-24 12:09:04 -0400

    Problem 1 finished

---
+++ temperature_category.py
@@ -0,0 +1,23 @@
+#Austin Hastings
+#CMP-131-86520
+#Week 5
+#Lab 1
+#24 Sep 2026
+#Problem 1 tempurture input
+print("------------------")
+print("Tempurture Problem")
+print("------------------")
+temperture= (float(input("Enter the tempature in Fahrenheit: ")))
+if (temperture >= 80):
+    print("It is Hot", temperture)
+elif (temperture >= 50 and temperture <=79 ):
+    print("It is Warm", temperture)
+else:
+    print("It is Cold", temperture)
+print()
+print("------------")
+print("Even or Odd")
+print("------------")
+number= (int(input("Enter an Integer: ")))
+
+
